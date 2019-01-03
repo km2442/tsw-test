52af467f3c221e22208c756e6af9038a448ddbfb
Unknown <urlikodd@gmail.com>
2019-01-03 15:48:19 +0100

    Initial commit

---
+++ src/components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
