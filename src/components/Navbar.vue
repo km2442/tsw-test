abd08d0fbe88e11a300cce34196884a527bd7671
Unknown <urlikodd@gmail.com>
2019-01-13 16:18:42 +0100

    Added navigation drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,26 @@
 <template>
-  <v-toolbar dark app color="primary">
-    <v-toolbar-side-icon></v-toolbar-side-icon>
-    <v-toolbar-title wrap class="white--text">Technologie sieci web</v-toolbar-title>
-    <v-spacer></v-spacer>
-  </v-toolbar>
+  <nav>
+    <v-toolbar dark app color="primary">
+      <v-toolbar-side-icon v-if="!drawer" @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-side-icon v-if="drawer" @click="drawer = !drawer">
+        <v-icon>close</v-icon>
+      </v-toolbar-side-icon>
+      <v-toolbar-title wrap class="white--text">Technologie sieci web</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-navigation-drawer v-model="drawer" app class="pa-3">
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon large class="primary--text">{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">{{link.text}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
@@ -11,7 +28,12 @@ export default {
   name: "Navbar",
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: "dashboard", text: "Strona główna", route: "/" },
+        { icon: "format_list_bulleted", text: "Test", route: "/test" },
+        { icon: "account_box", text: "Panel administracyjny", route: "/admin" }
+      ]
     };
   }
 };
