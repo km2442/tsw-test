34db17ea591e95ca714f0e4f3499b346d510e60c
Unknown <urlikodd@gmail.com>
2019-01-24 11:31:39 +0100

    Added paralax to index

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -10,14 +10,22 @@
         </v-carousel-item>
       </v-carousel>
     </div>
+    <div>
+      <v-parallax src="imgs/carousel-1.jpg">
+        <v-layout align-center column justify-center>
+          <h1 class="display-2 font-weight-thin mb-3">Quiz</h1>
+          <h4 class="subheading">Z Technologii Sieci WEB</h4>
+        </v-layout>
+      </v-parallax>
+    </div>
     <div class="mx-3">
       <v-btn
-      block
-      round
-      color="green darken-3 my-3"
-      router
-      :to="{ name: 'Instruction'}"
-    >Przejdź do instrukcji wykonania testu</v-btn>
+        block
+        round
+        color="green darken-3 my-3"
+        router
+        :to="{ name: 'Instruction'}"
+      >Przejdź do instrukcji wykonania testu</v-btn>
     </div>
   </div>
 </template>
