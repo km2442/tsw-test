fbd36bca5dc0102cff10e358806d5223a9b5a379
Unknown <urlikodd@gmail.com>
2019-02-04 16:05:07 +0100

    Centered footer text

--- src/components/Navigation/Footer.vue
+++ src/components/Navigation/Footer.vue
@@ -2,7 +2,9 @@
   <v-footer app height="auto">
     <v-card class="flex" tile>
       <v-card-title class="white--text blue-grey darken-2">
-        <strong class="subheading">Copyright &copy; 2019 Politechnika Rzeszowska Autor: Kamil Klecha</strong>
+        <v-spacer></v-spacer>
+        <strong class="subheading">Copyright &copy; 2019 Politechnika Rzeszowska, Autor: Kamil Klecha</strong>
+        <v-spacer></v-spacer>
       </v-card-title>
     </v-card>
   </v-footer>
