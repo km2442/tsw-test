369e3420d62829f6c6f2279a97a5d926ad64ded2
Kamil <urlikodd@gmail.com>
2019-08-10 20:06:53 +0200

    Updated & clear project
    updated dependencies

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
-import './plugins/vuetify'
+import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import { store } from './store/store'
 import vueSmoothScroll from 'vue2-smooth-scroll'
 
-Vue.use(vueSmoothScroll)
+Vue.use(vueSmoothScroll);
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
