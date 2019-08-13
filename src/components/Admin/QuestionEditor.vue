b86f9c55c94bf0e89f3bd5fad8de7f1c15ee8205
Kamil <urlikodd@gmail.com>
2019-08-14 00:09:53 +0200

    Removed unneeded mixin

--- src/components/Admin/QuestionEditor.vue
+++ src/components/Admin/QuestionEditor.vue
@@ -194,20 +194,94 @@
 
 <script>
 /* eslint-disable no-console */
-import { qedit } from "../Mixins/questionEditScript";
+import firebase from "../../firebase/init";
 export default {
-  mixins: [qedit],
   props: ["buttons", "preloadedQuestion"],
+  data() {
+    return {
+      Question: "",
+      Ans1: "",
+      Ans2: "",
+      Ans3: "",
+      Ans4: "",
+      GoodAns: [false, false, false, false],
+      Textarea: "",
+      Image: "",
+      selectedFile: "",
+      uploadState: 0,
+      formState: false,
+      rules: {
+        required: value => !!value || "Pole wymagane"
+      }
+    };
+  },
   created() {
     console.log(this.preloadedQuestion);
-      this.Question = this.preloadedQuestion.Question;
-      this.Ans1 = this.preloadedQuestion.Ans1;
-      this.Ans2 = this.preloadedQuestion.Ans2;
-      this.Ans3 = this.preloadedQuestion.Ans3;
-      this.Ans4 = this.preloadedQuestion.Ans4;
-      this.GoodAns = this.preloadedQuestion.GoodAns;
-      this.Textarea = this.preloadedQuestion.Textarea;
-      this.Image = this.preloadedQuestion.Image;
+    this.Question = this.preloadedQuestion.Question;
+    this.Ans1 = this.preloadedQuestion.Ans1;
+    this.Ans2 = this.preloadedQuestion.Ans2;
+    this.Ans3 = this.preloadedQuestion.Ans3;
+    this.Ans4 = this.preloadedQuestion.Ans4;
+    this.GoodAns = this.preloadedQuestion.GoodAns;
+    this.Textarea = this.preloadedQuestion.Textarea;
+    this.Image = this.preloadedQuestion.Image;
+  },
+  methods: {
+    prepareTextArea(text) {
+      return text.split("\\n");
+    },
+    clearAddForm() {
+      this.Question = "";
+      this.Ans1 = "";
+      this.Ans2 = "";
+      this.Ans3 = "";
+      this.Ans4 = "";
+      this.GoodAns = [false, false, false, false];
+      this.Textarea = "";
+      this.Image = "";
+      this.selectedFile = "";
+      this.uploadState = 0;
+    },
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0];
+    },
+    uploadImage() {
+      let storage = firebase.storage();
+      let img = String(Date.now()) + "_" + this.selectedFile.name;
+      let task = storage.ref("images/" + img).put(this.selectedFile);
+      let vm = this;
+      task.on(
+        "state_changed",
+        function(snapshot) {
+          let precentage =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          vm.uploadState = precentage;
+        },
+        function error(e) {
+          throw console.error(e);
+        },
+        function() {
+          task.snapshot.ref.getDownloadURL().then(downloadURL => {
+            vm.Image = downloadURL;
+          });
+        }
+      );
+    },
+    acceptQuestion() {
+      this.$emit("acceptQuestion", {
+        Question: this.Question,
+        Ans1: this.Ans1,
+        Ans2: this.Ans2,
+        Ans3: this.Ans3,
+        Ans4: this.Ans4,
+        GoodAns: this.GoodAns,
+        Textarea: this.Textarea,
+        Image: this.Image
+      });
+    },
+    cancelEdit() {
+      this.$router.push({ name: "Admin" });
+    }
   }
 };
 </script>
