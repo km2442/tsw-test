12703fd965a574e51cc45090c8be7d1b48286f32
Kamil <urlikodd@gmail.com>
2019-08-17 19:53:07 +0200

    Added login and logout

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,19 +1,58 @@
 import axios from "axios"
 
 const state = {
+    idToken: null,
+    userId: null,
     user: null
 }
 
 const mutations = {
-    changeUser(state, user) {
-        state.user = user
-    }
+    authUser (state, userData) {
+        state.idToken = userData.token
+        state.userId = userData.userId
+      },
+      clearAuthData (state) {
+        state.idToken = null
+        state.userId = null
+      }
 }
 
 const actions = {
     changeUser({ commit }, user) {
         commit("changeUser", user);
     },
+    setLogoutTimer({ commit }, expirationTime) {
+        setTimeout(() => {
+            commit('clearAuthData')
+        }, expirationTime * 1000)
+    },
+    login({ commit, dispatch }, authData) {
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC7TWgPiFeplz8-ZU8Zl936-vEbL1zPyJk', {
+            email: authData.email,
+            password: authData.password,
+            returnSecureToken: true
+        })
+            .then(res => {
+                const now = new Date()
+                const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+                localStorage.setItem('token', res.data.idToken)
+                localStorage.setItem('userId', res.data.localId)
+                localStorage.setItem('expirationDate', expirationDate)
+                commit('authUser', {
+                    token: res.data.idToken,
+                    userId: res.data.localId
+                })
+                dispatch('setLogoutTimer', res.data.expiresIn)
+            })
+            .catch(error => console.log(error))
+    },
+    logout ({commit}) {
+        commit('clearAuthData')
+        localStorage.removeItem('expirationDate')
+        localStorage.removeItem('token')
+        localStorage.removeItem('userId')
+        router.replace('/')
+      },
 }
 
 const getters = {
