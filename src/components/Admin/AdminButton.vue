d25fd28e4e1f12922b4b788f54c1a1cf437d63da
Kamil <urlikodd@gmail.com>
2020-06-12 14:44:45 +0200

    Standarized code

--- src/components/Admin/AdminButton.vue
+++ src/components/Admin/AdminButton.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <v-card class="mx-5 my-2" :color="color" style="cursor: pointer;">
+    <v-card class="mx-5 my-2 rounded-lg" v-ripple :color="color" style="cursor: pointer;">
       <v-container fill-height fluid>
         <v-row align="center" justify="center">
           <v-card-title>
             <v-icon large left>{{ icon }}</v-icon>
-            <span class="title text-center font-weight-bold">{{ text.toUpperCase() }}</span>
+            <span
+              class="text-h6 text-center text-uppercase font-weight-bold"
+            >{{ text }}</span>
           </v-card-title>
         </v-row>
       </v-container>
