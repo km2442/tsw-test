a397878ea2d0dca32a15eae350af0f498b6407eb
Kamil <urlikodd@gmail.com>
2020-06-12 14:41:25 +0200

    Setted locale to pl

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,8 +3,15 @@ import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
+import pl from 'vuetify/es5/locale/pl';
+import en from 'vuetify/es5/locale/en';
+
 export default new Vuetify({
   icons: {
     iconfont: 'mdi',
   },
+  lang: {
+    locales: { pl, en },
+    current: 'pl',
+  }
 });
